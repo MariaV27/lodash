4da1bf8c79c13f3082479b752780c0b39b711a2e
Maria Vintimilla <mariavintimilla@Marias-MacBook-Air.local>
2016-04-12 19:13:54 -0400

    example1

---
+++ example1.js
@@ -0,0 +1,29 @@
+var studentsOfDoom = [
+        {
+            "name": "Bern",
+            "lairs": [{"name":"Washington"}, {"name": "Vermont"}, {"name": "Brooklyn"}]
+        },
+        {
+            "name": "Hillary",
+            "lairs": [{"name":"Washington"}, {"name": "New York"}, {"name": "Arkansas"}]
+        },
+        {
+            "name": "Trump",
+            "lairs": [{"name":"New York"}]
+        },
+        {
+            "name": "Cruz",
+            "lairs": [{"name": "Washington"}, {"name": "New York"}]
+        }
+    ]
+
+for(var i = 0; i < presidentialCandidatesofDoom.length; i++) {
+    console.log(presidentialCandidatesofDoom[i].lairs[0].name);
+}
+
+
+//Washington
+//Washington
+//New York
+//Washington
+
