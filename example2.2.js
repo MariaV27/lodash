957a1a1336c9a5f14df6f786c52b8077e4225cda
Maria Vintimilla <mariavintimilla@Marias-MacBook-Air.local>
2016-04-12 20:20:17 -0400

    2.2.

---
+++ example2.2.js
@@ -0,0 +1,27 @@
+var _ = require("lodash");
+// for( var i =0; i < 10; i++){
+//     console.log(Math.floor(Math.random(100) * 100));
+// }
+
+// You can't print the same number twice
+
+
+var numberCount = 100;
+var randomArray = [];
+var steps = 0;
+
+_.times(100, function(){
+    var random = _.random(100);
+
+    if(randomArray.length == 0) {
+      randomArray.push(random);
+    } else{
+      while(tempRandomNumbers.indexOf(random) != -1){
+        random = _.random(100);
+      }
+      randomArray.push(random);
+    }
+ })
+
+console.log(randomArray);
+console.log(randomArray.length);
